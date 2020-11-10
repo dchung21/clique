e6fcf9ee58f618d5e6bccff38693af0a0490e033
James Ma <james168ma@gmail.com>
2020-11-10 07:31:52 -0800

    added simple signed in page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Login.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-		<LoginPage />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Switch>
+            <Route path="/signedIn">
+              <SignedInPage />
+            </Route>
+            <Route path="/">
+              <LoginPage />
+            </Route>
+        	</Switch>
+        </header>
+      </div>
+    </Router>
   );
+}
+
+function SignedInPage() {
+	return <h2>You successfully signed in</h2>;
 }
 
 export default App;
