971eeee924afd8fe3ba0299d5047bf0a94ecbccb
James Ma <james168ma@gmail.com>
2020-11-10 20:47:00 -0800

    added a textarea in profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,6 +7,7 @@ import { FIREBASE_CONFIG } from '../config.js';
 
 export default function Profile(props) {
 	const [image, setImage] = useState("");
+	const [description, setDescription] = useState("");
 
 	//this is probably hacky code and will be prone to crashing
 	const auth = firebase.auth();
@@ -23,6 +24,11 @@ export default function Profile(props) {
 		}
 	}
 
+	//on typing of words into textarea
+	const handleTextChange = (event) => {
+		setDescription(event.target.value);
+	}
+
 	//TODO 
 	//this will upload image to firebase storage, I want to hash it with userID
 	const handleUpload = (event) => {
@@ -33,11 +39,32 @@ export default function Profile(props) {
 		const upload = storage.ref(`pix/test`).put(image);
 	}
 
+	//TODO
+	//this will upload text to firebase cloud
+	const handleTextUpload = (event) => {
+
+		//only length of 10 or less
+		if(description.split(" ").length > 10) {
+			alert("Your description is too long!");
+		} else {
+			alert("You just updated your description with: " + description);
+		}
+
+		//prevent from reloading page on submission
+		event.preventDefault();
+	}
+
 	return (
 		<div>
 			<Link to='/home'>Home</Link>
+			<h2>Upload an image!</h2>
 			<form>
 				<input type="file" onChange = {onChange} />
+			</form>
+			<h2>Write something that describes you (10 words max)</h2>
+			<form onSubmit={handleTextUpload}>
+				<textarea value={description} onChange={handleTextChange} />
+				<input type="submit" value="Submit" />
 			</form>
 		</div>
 	)
