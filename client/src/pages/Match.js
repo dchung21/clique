39aa8aa8fa91f26c53f4bfbfe9b848ec9fbdca67
Justin Pau <jupau@berkeley.com>
2020-11-11 15:18:25 -0800

    UI for Match Page

--- client/src/pages/Match.js
+++ client/src/pages/Match.js
@@ -3,34 +3,54 @@ import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import HomeIcon from '@material-ui/icons/Home'; 
-import ChatIcon from '@material-ui/icons/Chat';
+import ChatIcon from '@material-ui/icons/Chat'; 
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import IconButton from '@material-ui/core/IconButton';
+import "./Button.css"
 
-export default function Match() {
+//export default function Match() {
     function Header() {
-         
+        <div className = "button">
+            <Link to='/home'>
+                <IconButton>
+                    <HomeIcon />
+                </IconButton>
+            </Link>
+        </div>
+        //<Link to='/home'>
+            //<Button variant = "primary">
+                //<HomeIcon />
+            //</Button>
+        //</Link>
+    //</div>
+    } 
+    function tailer() {
+        <div className = "button">
+            <Link to='/chat'>
+                <IconButton>
+                    <ChatIcon />
+                </IconButton>
+            </Link>
+
+            <NavigateNextIcon/>
+        </div>
     }
 
-	return (
-        <Container className="d-flex justify-content-center align-items-center min-vh-100">
-            <div>
-                <Link to='/home'>
-                    <Button variant = "primary">
-                        <HomeIcon />
-                    </Button>
-                </Link>
+	//return (
+        //<Container className="d-flex justify-content-center align-items-center min-vh-100">
+            //<div className = "button">
+                //<Link to='/chat'>
+                    //<Button variant = "primary">
+                        //<ChatIcon />
+                    //</Button>
+                //</Link>
 
-                <h2>Matching Page</h2>
+                //<Button variant = "outline-secondary">next</Button>{' '}
+            //</div>
+        //</Container>
+    //)
 
-                <Link to='/chat'>
-                    <Button variant = "primary">
-                        <ChatIcon />
-                    </Button>
-                </Link>
-
-                <Button variant = "outline-secondary">next</Button>{' '}
-            </div>
-        </Container>
-	)
+    //<h2>Matching Page</h2>
     
     
-}
+//}
