39baeacb42fde22f82f4a253d9da28a90406307d
Justin Pau <jupau@berkeley.com>
2020-11-11 13:52:09 -0800

    UI for Match Page

--- client/src/pages/Match.js
+++ client/src/pages/Match.js
@@ -6,18 +6,18 @@ import Container from 'react-bootstrap/Container'
 export default function Match() {
 
 	return (
-        //<Container className="d-flex justify-content-center align-items-center min-vh-100">
+        <Container className="d-flex justify-content-center align-items-center min-vh-100">
             <div>
-                <Button variant = "primary">
-                    <Link to='/home'>Home</Link>
-                </Button>{' '}
+                <Link to='/home'>
+                    <Button variant = "primary">Home</Button>
+                </Link>
                 <h2>Matching Page</h2>
-                <Button variant = "primary">
-                    <Link to='/chat'>Chat</Link>
-                </Button>{' '}
+                <Link to='/chat'>
+                    <Button variant = "primary">Chat</Button>
+                </Link>
                 <Button variant = "outline-secondary">next</Button>{' '}
             </div>
-        //</Container>
+        </Container>
 	)
     
     
