e64588ed87b778bfaae0cd6c312914a5e62265be
James Ma <james168ma@gmail.com>
2020-11-10 21:03:56 -0800

    changed to better variable names

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,7 +7,7 @@ import { FIREBASE_CONFIG } from '../config.js';
 
 export default function Profile(props) {
 	const [image, setImage] = useState("");
-	const [description, setDescription] = useState("");
+	const [bio, setBio] = useState("");
 
 	//this is probably hacky code and will be prone to crashing
 	const auth = firebase.auth();
@@ -26,7 +26,7 @@ export default function Profile(props) {
 
 	//on typing of words into textarea
 	const handleTextChange = (event) => {
-		setDescription(event.target.value);
+		setBio(event.target.value);
 	}
 
 	//TODO 
@@ -40,14 +40,14 @@ export default function Profile(props) {
 	}
 
 	//TODO
-	//this will upload text to firebase cloud
-	const handleTextUpload = (event) => {
+	//this will upload bio to cloud firestore
+	const handleBioUpload = (event) => {
 
 		//only length of 10 or less
-		if(description.split(" ").length > 10) {
+		if(bio.split(" ").length > 10) {
 			alert("Your description is too long!");
 		} else {
-			alert("You just updated your description with: " + description);
+			alert("You just updated your description with: " + bio);
 		}
 
 		//prevent from reloading page on submission
@@ -62,8 +62,8 @@ export default function Profile(props) {
 				<input type="file" onChange = {onChange} />
 			</form>
 			<h2>Write something that describes you (10 words max)</h2>
-			<form onSubmit={handleTextUpload}>
-				<textarea value={description} onChange={handleTextChange} />
+			<form onSubmit={handleBioUpload}>
+				<textarea value={bio} onChange={handleTextChange} />
 				<input type="submit" value="Submit" />
 			</form>
 		</div>
