c6a3184e8ce3144070e6994c9e573461c02d6a8b
Justin Pau <jupau@berkeley.com>
2020-11-11 15:57:16 -0800

    match revert

--- client/src/pages/Match.js
+++ client/src/pages/Match.js
@@ -6,70 +6,53 @@ import HomeIcon from '@material-ui/icons/Home';
 import ChatIcon from '@material-ui/icons/Chat'; 
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import IconButton from '@material-ui/core/IconButton';
-//import "../Button.css"
+import "../Button.css"
 
 export default function Match() {
-    function Header() {
-        <div className = "button">
-            <Link to='/home'>
-                <IconButton>
-                    <HomeIcon />
-                </IconButton>
-            </Link>
-        </div>
-        //<Link to='/home'>
-            //<Button variant = "primary">
-                //<HomeIcon />
-            //</Button>
-        //</Link>
-    //</div>
-    } 
-    function tailer() {
-        <div className = "button">
-            <Link to='/chat'>
-                <IconButton>
-                    <ChatIcon />
-                </IconButton>
-            </Link>
-
-            <NavigateNextIcon/>
-        </div>
-    }
-
 	return (
-        <div>
-            <div>
+        
+        <Container className="d-flex justify-content-center align-items-center min-vh-100">
+            <div className = "button">
                 <Link to='/home'>
-                    <IconButton>
-                        <HomeIcon />
-                    </IconButton>
+                    <Button variant = "primary">
+                        home
+                    </Button>
                 </Link>
             </div>
-            <div>
-            <Link to='/chat'>
-                <IconButton>
-                    <ChatIcon />
-                </IconButton>
-            </Link>
+            <h1> Match Page</h1>
+            <div className = "button">
+                <Link to='/chat'>
+                    <Button variant = "primary">
+                        chat
+                    </Button>
+                </Link>
 
-            <NavigateNextIcon/>
+                <Button variant = "outline-secondary">next</Button>
             </div>
-
-        </div>
-        //<Container className="d-flex justify-content-center align-items-center min-vh-100">
-            //<div className = "button">
-                //<Link to='/chat'>
-                    //<Button variant = "primary">
-                        //<ChatIcon />
-                    //</Button>
-                //</Link>
-
-                //<Button variant = "outline-secondary">next</Button>{' '}
-            //</div>
-        //</Container>
+        </Container>
     )
 
     //<h2>Matching Page</h2>
     
     
 }
+
+//<div>
+            //<h1>spain without the s</h1>
+            /* <ChatIcon />
+            <div>
+                <Link to='/home'>
+                    <IconButton>
+                        <HomeIcon />
+                    </IconButton>
+                </Link>
+            <Link to='/chat'>
+                <IconButton>
+                    <ChatIcon />
+                </IconButton>
+            </Link>
+
+            <NavigateNextIcon/>
+            </div> */
+
+        //</div>
