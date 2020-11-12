23382b12df06bbee0659bf665a5ee09b8acab8ca
James Ma <james168ma@gmail.com>
2020-11-11 22:10:00 -0800

    added realtime chat

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
@@ -8,22 +8,30 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 export default function Chat(props) {
-    const [messagesRef, setMessagesRef] = useState("");
-
     const auth = firebase.auth();
     const firestore = firebase.firestore();
 
-    if(this.props.location.state.convoRef) {
-        setMessagesRef(firestore.collection(this.props.location.state.convoRef));
-    }
-    
+    const messagesRef = firestore.collection(props.location.state.convoRef);
+    const query = messagesRef.orderBy('timestamp_ms', 'desc').limit(50);
 
+    const [messages] = useCollectionData(query, {idField: 'id'});
+
+    console.log("convoRef: ", props.location.state.convoRef);
 
 	return (
         <div>
             <Link to='/home'>Home</Link>
             <h2>Chat</h2>
+            <div>
+                {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+            </div>
         </div>
 	)
 	
+}
+
+function ChatMessage(props) {
+    const { content, uid } = props.message;
+
+    return <p>{content}</p>
 }
