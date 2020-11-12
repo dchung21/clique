7e6f79160f5b134fc74cc514ec81e375fa46a7c8
James Ma <james168ma@gmail.com>
2020-11-11 20:51:30 -0800

    added auto redirecting

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "./contexts/AuthContext.js";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import PrivateRoute from "./PrivateRoute.js";
 
@@ -14,10 +14,17 @@ import Convos from "./pages/Convos.js";
 import Chat from "./pages/Chat.js";
 import Profile from "./pages/Profile.js";
 
+import firebase from 'firebase/app'
 import "firebase/firestore";
 import "firebase/auth";
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 function App() {
+  const auth = firebase.auth();
+
+  const [user] = useAuthState(auth)
+
   return (
     
       <Container className = "d-flex align-items-center justify-content-center"
@@ -27,7 +34,8 @@ function App() {
         <Router>
           <AuthProvider>
             <Switch>
-                
+              
+
               <Route path = "/signup" component = {Signup} />
               <Route path = "/login" component = {Login} />
                 
@@ -36,6 +44,10 @@ function App() {
               <PrivateRoute path = "/convos" component = {Convos} />
               <PrivateRoute path = "/chat" component = {Chat} />
               <PrivateRoute path = "/profile" component = {Profile} />
+
+              <Route exact path="/">
+                {user ? <Redirect to="/home"/> : <Redirect to="/login"/>}
+              </Route>
 
             </Switch>
           </AuthProvider>
--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
-export default function Chat() {
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+export default function Chat(props) {
+    const [messagesRef, setMessagesRef] = useState("");
+
+    const auth = firebase.auth();
+    const firestore = firebase.firestore();
+
+    if(this.props.location.state.convoRef) {
+        setMessagesRef(firestore.collection(this.props.location.state.convoRef));
+    }
+    
+
 
 	return (
         <div>
