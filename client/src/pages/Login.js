ed1a36b914cb3c9435cd9b59ec82380be859e2cc
James Ma <james168ma@gmail.com>
2020-11-11 20:57:26 -0800

    fixed signup and login layout

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -28,7 +28,7 @@ export default function Login() {
     }
 
     return (
-        <>
+        <div>
             <Card style = {{ width: "400px" }}>
                 <Card.Body>
                     <h2 className = "text-center mb-4">Log In</h2>
@@ -51,6 +51,6 @@ export default function Login() {
             <div className = "w-100 text-center mt-2">
                 Need an account? <Link to ="signup">Sign Up</Link>
             </div>
-        </>
+        </div>
     )
 }
