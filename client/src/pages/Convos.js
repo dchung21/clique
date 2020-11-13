7021f0dced008df40892fd5609e795cd1dc615b5
dchung21 <dchung679@gmail.com>
2020-11-12 16:54:39 -0800

    conversation bug fix

--- client/src/pages/Convos.js
+++ client/src/pages/Convos.js
@@ -45,7 +45,6 @@ export default function Convos() {
                             //setConvos(convoBuffer);
                             count++;
                         });
-                        console.log(convoBuffer)
                         setConvos(convoBuffer)
                         if(count > 1) {
                             console.log("something went wrong, more than one message.");
@@ -63,8 +62,64 @@ export default function Convos() {
     }
 
     useEffect(() => {
+         function resolveMe(docId, uid) {
+            return new Promise(resolve => {
+                fs.collection("conversations/" + docId + "/messages").orderBy("timestamp", "desc").limit(1).get().then(function(messageSnapshot) {
+                             messageSnapshot.forEach(function(messageDoc) {
+                                 messageSnapshot.forEach(function(messageDoc) {
+                                     storage.ref("pix").child(uid).getDownloadURL().then(url => {
+                                         let obj = {
+                                            imgURL: url,
+                                            content: messageDoc.data().content,
+                                            timestamp: messageDoc.data().timestamp,
+                                            convoRef: "conversations/" + docId + "/messages"
+                                         }
+                                         resolve(obj);
+                                     })
+                                 })
+                             })
+                    })
+            });
+         }
+
+         async function childMsgFetch(uids, docId) {
+                 let convoBuffer = [];
+                 for (let i in docId) {
+                     let conversation = await resolveMe(docId[i], uids[i]);
+                     convoBuffer.push(conversation);
+                 }
+                 let count = convoBuffer.length;
+                 if(count > 1) {
+                            console.log("something went wrong, more than one message.");
+                 } else if(count === 1) {
+                            console.log("successful got one message.");
+                } else {
+                            console.log("something wierd happened");
+                 }
+                 setLoaded(loaded+1);
+                 setConvos(convoBuffer);
+        }
 		 async function fetchData() {
-             
+            const result = await firebase.auth().onAuthStateChanged(function(user) {
+                if (user != null) {
+                    fs.collection("conversations").where("uid1", "==", user.uid).get().then(function(snapshot1) {
+                        fs.collection("conversations").where("uid2", "==", user.uid).get().then(function(snapshot2) {
+                            let uids = [];
+                            let docId = [];
+                            snapshot1.forEach(function(doc) {
+                                 uids.push(doc.data().uid2);
+                                 docId.push(doc.id);
+                            })
+                            snapshot2.forEach(function(doc) {
+                                uids.push(doc.data().uid1);
+                                docId.push(doc.id);
+                            })
+                            childMsgFetch(uids, docId);
+                        })
+                    })
+                }
+            })
+            /*
 			await firebase.auth().onAuthStateChanged(async function(user) {
 				if (user != null) {
                     setUid(user.uid);
@@ -78,6 +133,7 @@ export default function Convos() {
 				else
 					console.log("not logged in");
 			});
+            */
 		}
         
         fetchData();
