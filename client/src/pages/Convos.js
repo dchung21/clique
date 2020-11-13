5843d547cd451d4c0b4a9a122aedcf448de61421
dchung21 <dchung679@gmail.com>
2020-11-12 16:56:06 -0800

    removing old code

--- client/src/pages/Convos.js
+++ client/src/pages/Convos.js
@@ -18,49 +18,6 @@ export default function Convos() {
 
     let convoBuffer = [];
 
-    async function getConvos(user, uidTarget) {
-        // get convos from uidTarget
-        fs.collection("conversations").where(uidTarget, "==", user.uid)
-        .get()
-        .then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                let otherUid = (uidTarget === "uid1") ? doc.data().uid2 : doc.data().uid1;
-                //get the images for the convos of the other person
-                storage.ref("pix").child(otherUid).getDownloadURL().then(url => {
-                    
-                    //get the last message
-                    fs.collection("conversations/" + doc.id + "/messages").orderBy("timestamp", "desc").limit(1)
-                    .get()
-                    .then(function(messageSnapshot) {
-                        let count = 0;
-                        messageSnapshot.forEach(function(messageDoc) {
-                            // pushes into convo buffer
-                            convoBuffer.push({
-                                imgURL: url,
-                                content: messageDoc.data().content,
-                                timestamp: messageDoc.data().timestamp,
-                                convoRef: "conversations/" + doc.id + "/messages"
-                            })
-                            // setting the convos after convoBuffer??
-                            //setConvos(convoBuffer);
-                            count++;
-                        });
-                        setConvos(convoBuffer)
-                        if(count > 1) {
-                            console.log("something went wrong, more than one message.");
-                        } else if(count === 1) {
-                            console.log("successful got one message.");
-                        } else {
-                            console.log("something wierd happened");
-                        }
-                    });
-                })
-            })
-            setLoaded(loaded+1);
-           // console.log("loaded: ",loaded);
-        })
-    }
-
     useEffect(() => {
          function resolveMe(docId, uid) {
             return new Promise(resolve => {
@@ -119,21 +76,7 @@ export default function Convos() {
                     })
                 }
             })
-            /*
-			await firebase.auth().onAuthStateChanged(async function(user) {
-				if (user != null) {
-                    setUid(user.uid);
-                    // I think it may be a concurrency issue??
-                    await getConvos(user, "uid1")
-                    await getConvos(user, "uid2")
-                    // right here idk
-                    //setConvos(convoBuffer)
-                    console.log("convoBuffer in fetchData: ", convoBuffer);
-				}
-				else
-					console.log("not logged in");
-			});
-            */
+
 		}
         
         fetchData();
