814c9a43f797c4195d48454ce0631928e68b0474
James Ma <james168ma@gmail.com>
2020-11-12 18:33:16 -0800

    added loading to convos

--- client/src/pages/ConvoContainer.js
+++ client/src/pages/ConvoContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/esm/ListGroup';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/esm/Container';
+import Spinner from 'react-bootstrap/Spinner';
 import ConvoItem from './ConvoBox.js';
 
 export default function ConvoContainer(props) {
@@ -10,7 +11,9 @@ export default function ConvoContainer(props) {
 	let content;
 	console.log("container", convos)
 	if (loaded < 1)
-		content = <h3>...</h3>
+		content = <>
+			<Spinner animation="border" variant="primary" />
+		</>
 	else if (loaded >= 1 && convos.length === 0)
 		content = <h3>You have no conversations :(</h3>
 	else {
@@ -24,14 +27,6 @@ export default function ConvoContainer(props) {
 		);
 	}
 
-	return (
-	<Container className="d-flex justify-content-center align-items-center min-vh-100">
-            <Container className="w-75">
-                <Link to='/home'>Home</Link>
-                <h2>Convos</h2>
-				{content}
-            </Container>
-        </Container>
-	);
+	return ( content );
 }
         
